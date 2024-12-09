58fca1d3a58fb87e92f454924d69a72b1554428c
Saray Rodríguez <rodriguezsaray602@gmail.com>
2024-12-09 23:39:44 +0000

    mi generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Un bebé", "Una señora", "Un perro", "Mi tío", "Un payaso"];
+  let action = ["mojó", "rompió", "pateó", "robó"];
+  let what = ["mi computadora", "mi proyecto", "mi teclado", "mi mouse"];
+  let when = [
+    "en el centro comercial",
+    "en la universidad",
+    "en el bondi",
+    "en mi camino a casa",
+    "en el starbucks"
+  ];
+
+  function partExcuseGenerator(part) {
+    return part[Math.floor(Math.random() * part.length)];
+  }
+
+  let whoExcuse = partExcuseGenerator(who);
+  let actionExcuse = partExcuseGenerator(action);
+  let whatExcuse = partExcuseGenerator(what);
+  let whenExcuse = partExcuseGenerator(when);
+  let excuse =
+    whoExcuse + " " + actionExcuse + " " + whatExcuse + " " + whenExcuse + ".";
+
+  document.getElementById("excuse").innerText = excuse;
+
+  document.getElementById("generate").onclick = function() {
+    let whoExcuse = partExcuseGenerator(who);
+    let actionExcuse = partExcuseGenerator(action);
+    let whatExcuse = partExcuseGenerator(what);
+    let whenExcuse = partExcuseGenerator(when);
+    let excuse =
+      whoExcuse +
+      " " +
+      actionExcuse +
+      " " +
+      whatExcuse +
+      " " +
+      whenExcuse +
+      ".";
+    document.getElementById("excuse").innerText = excuse;
+  };
 };
